5e566d33239c4222ae31190d533c1e59cadbe177
Sukumar <shilpa.sukumar@accenture.com>
2021-01-20 15:42:13 +0530

    demo for grpc

---
+++ grpc-demo/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"basic-grpc-master/chat"
+	"chat"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/golang/protobuf/protoc-gen-go/grpc/Grpc/chat"
+)
+
+func main() {
+
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from the client!",
+	}
+
+	response, err := c.SayHello(context.Background(), &chat.Message{Body: "Hello From Client!"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Body)
+
+}
